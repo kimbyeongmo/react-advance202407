6fd910d039b8c49fd595b0d20b7b85b6a3ddaf45
kimbyeongmo <kbmomo9999@naver.com>
2024-07-25 11:17:52 +0900

    # key를 이용한 컴포넌트 초기화

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
       <Header />
       <main>
         <ConfigureCounter onSet={setCountHandler}/>
-        <Counter initialCount={chosenCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
       </main>
     </>
   );
