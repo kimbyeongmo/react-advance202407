e23d5010b1dde6d6fbd9fa738674eac84409221e
kimbyeongmo <kbmomo9999@naver.com>
2024-07-30 08:54:12 +0900

    # 카트에서 상품 제거 상태관리

--- src/redux-cart/components/Cart/CartButton.js
+++ src/redux-cart/components/Cart/CartButton.js
@@ -1,20 +1,22 @@
 import React from "react";
 import styles from './CartButton.module.css';
 import { uiActions } from "../../store/ui-slice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const CartButton = () => {
+
+  const totalQuantity = useSelector(state => state.cart.totalQuantity);
 
   const dispatch = useDispatch();
 
   const toggleCartHandler = e => {
-    dispatch(uiActions.toggle());  
+    dispatch(uiActions.toggle());
   };
 
   return (
     <button className={styles.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={styles.badge}>0</span>
+      <span className={styles.badge}>{totalQuantity}</span>
     </button>
   );
 };
--- src/redux-cart/store/cart-slice.js
+++ src/redux-cart/store/cart-slice.js
@@ -14,13 +14,30 @@ const cartSlice = createSlice({
       const newCartItem = action.payload;
       // 이 상품이 기존 장바구니배열에 있는 상품인지 탐색
       const existingItem = state.cartItems.find(item => item.id === newCartItem.id);
-      
-      if(!existingItem){// 처음 장바구니에 추가된 상품인 경우
+
+      if (!existingItem) { // 처음 장바구니에 추가된 상품인 경우
         state.cartItems.push(newCartItem);
-      }else {
+      } else {
         existingItem.quantity++;
-        existingItem.total+=newCartItem.price;
+        existingItem.total += newCartItem.price;
       }
+      state.totalQuantity++;
+    },
+    removeCartItem(state, action) {
+      const id = action.payload;
+      const existingItem = state.cartItems.find(item => item.id === id);
+
+      if (existingItem.quantity === 1) { // 수량이 1인 경우 배열에서 삭제
+        // const index = state.cartItems.findIndex(item => item.id === id);
+        // state.cartItems.splice(index, 1);
+
+        state.cartItems = state.cartItems.filter(item => item.id !== id);
+
+      } else { // 수량, 총액 갱신
+        existingItem.quantity--;
+        existingItem.total -= existingItem.price;
+      }
+      state.totalQuantity--;
     }
   }
 });
